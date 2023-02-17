b5a35b47a78488d442b7d5cb3d165247bf2f6f69
MSI\antar <alaktalapatra@gmail.com>
2023-02-17 21:51:57 +0530

    17-02-23

---
+++ prac.25.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    switch(n){
+
+        default:
+        printf("Invalid input..");
+        break;
+        case :
+        printf("January");
+        break;
+        case 1:
+        printf("January");
+        break;
+        case 2:
+        printf("February");
+        break;
+        case 3:
+        printf("W");
+        break;
+        case 4:
+        printf("Thursday");
+        break;
+        case 5:
+        printf("Friday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+
+        //default:
+        //printf("Invalid input..");
+    }
+
+    return 0;
+}
