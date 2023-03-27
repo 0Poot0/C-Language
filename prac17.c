fb1010b967d10c92378d990b74fa7a2aaf7aaa9a
MSI\antar <alaktalapatra@gmail.com>
2023-03-27 23:39:35 +0530

    27-03-23

---
+++ prac17.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a number :");
+    scanf("%d",&num);
+    if(num>10)
+        printf("%d IS GREATER THAN 10",num);
+    
+    return 0;
+}
