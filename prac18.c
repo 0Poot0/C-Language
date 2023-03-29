89d2a34f1f2e4513e000b97f62a5ff5cd5f993b7
MSI\antar <alaktalapatra@gmail.com>
2023-03-29 22:48:31 +0530

    29-03-23

---
+++ prac18.c
@@ -0,0 +1,15 @@
+//Write a program to check wheather no is divisible by 5 or not....
+
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    if(num%5==0){
+        printf("%d is divisible by 5",num);
+    }
+    else{
+        printf("%d is not divisible by 5",num);
+    }
+    return 0;
+}
