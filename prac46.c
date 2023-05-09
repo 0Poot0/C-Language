6a07a2065140f24637d78a3f357dc14d2b4413da
MSI\antar <alaktalapatra@gmail.com>
2023-05-09 23:55:18 +0530

    09-05-23

---
+++ prac46.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a= 5643;
+    printf("%d\n",a);
+    printf("*%d\n",a);
+    printf("*%d*",a);
+    printf("*%-6d*\n",a);
+    printf("*%6d*\n",a);
+    printf("*%06d*\n",a);
+    printf("*%16d*\n",a);
+    return 0;
+}
