84387197a77a2398a6d54b07b275dcab28252116
MSI\antar <alaktalapatra@gmail.com>
2023-05-17 23:07:06 +0530

    17-05-23

---
+++ prac50.c
@@ -0,0 +1,11 @@
+//Multiplication of two numbers by using function (1. with RT and with parameters)..
+
+#include <stdio.h>
+int multiply(int a,int b);
+int main(){
+printf("%d",multiply(3,2));
+    return 0;
+}
+int multiply(int a, int b){
+    return a*b;
+}
