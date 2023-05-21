8ee9657e08fced18f1ac3cc92a175bacee576898
MSI\antar <alaktalapatra@gmail.com>
2023-05-21 22:47:18 +0530

    21-05-23

---
+++ prac52.c
@@ -0,0 +1,18 @@
+//WTP 
+#include<stdio.h>
+int display(int n){
+    if(n<1){
+        return 0;
+    }
+    else{
+        printf("%d",n);
+        display(n-1);
+        printf("%d",n);
+    }
+}
+int display(int n);
+int main(){
+int n=3;
+display(n);
+    return 0;
+}
