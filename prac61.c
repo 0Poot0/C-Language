2d585ca074cf4aebd9780c4346004e516627327c
MSI\antar <alaktalapatra@gmail.com>
2023-06-05 00:04:11 +0530

    05-08-23

---
+++ prac61.c
@@ -0,0 +1,20 @@
+//wtp to take an inout for 2d array and display an output
+
+#include<stdio.h>
+int main(){
+    int r1,r2,c1,c2;
+    printf("Enetr the no of rors in first matrix : ");
+    scanf("%d",&r1);
+
+printf("Enetr the no of columns in first matrix : ");
+    scanf("%d",&c1);
+
+printf("Enetr the no of rows in second matrix : ");
+    scanf("%d",&r2);
+
+printf("Enetr the no of colums in first matrix : ");
+    scanf("%d",&c2);
+
+    
+    return 0;
+}
