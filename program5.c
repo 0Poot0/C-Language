ea2100b75324bcff94a6268f32fa539a19895c5f
MSI\antar <alaktalapatra@gmail.com>
2023-07-25 23:14:10 +0530

    15-07-23

---
+++ program5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int a= 10;
+int b= 10;
+printf("Addition = %d",(a+b));
+printf("Subtraction = %d",(a-b));
+printf("Multiplication = %d",(a*b));
+printf("Division = %d",(a/b));
+printf("Modulud = %d",(a%b));
+
+printf("%d + %d = %d \n",a,b,(a+b));
+printf("%d - %d = %d \n",a,b,(a-b));
+printf("%d * %d = %d \n",a,b,(a*b));
+printf("%d / %d = %d \n",a,b,(a/b));
+printf("%d %% %d = %d \n",a,b,(a%b));
+
+
+return 0;
+}
+
