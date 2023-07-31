e60bf2387973553157fdcbd06b74cb091e666ced
MSI\antar <alaktalapatra@gmail.com>
2023-07-31 21:06:39 +0530

    31-07-23

---
+++ prac96.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int *i;
+    double *b;
+    float *f;
+    printf("%d\n",sizeof(b));
+    printf("%d\n",sizeof(b));
+    printf("%d\n",sizeof(f));
+    return 0;
+}
